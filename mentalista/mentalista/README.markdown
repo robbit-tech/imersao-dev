d50d786283afb18ace29ed084a93b0a75511ae25
robbit-tech <56116010+robbit-tech@users.noreply.github.com>
2021-04-09 14:06:04 -0300

    Add files via upload
    Adicionado mais um projeto mentalista

---
+++ mentalista/mentalista/README.markdown
@@ -0,0 +1,5 @@
+# Mentalista
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/robbit-tech/pen/bGgVjgv](https://codepen.io/robbit-tech/pen/bGgVjgv).
+
+
---
+++ mentalista/mentalista/dist/script.js
@@ -0,0 +1,35 @@
+var numero = parseInt(Math.random() * 10) 
+var tentativas = 3
+var check = 0
+
+
+
+while (tentativas > 0 && check != numero)  {
+  
+  var chute = parseInt(prompt("Digite um numero de 1 a 10 "))
+  if (numero == chute){
+    console.log("Parabens voce acertou!" )
+    check = chute
+    
+      
+  }else if (numero > chute)  {
+    alert (" o numero é maior")
+    console.log(" Você errou ")
+    tentativas = tentativas - 1
+    check = chute
+    
+    
+  }else if (numero < chute)  {
+    alert ("o numero é menor")
+    tentativas = tentativas - 1
+    check = chute       
+  }
+    
+  
+}
+if (chute != numero){
+      alert ("Suas tentativas acabaram, voce perdeu o numero correto era " + numero )    
+    }else{
+      alert ("Fim de jogo acertou em " + tentativas + "  tentaivas")
+      
+    }
