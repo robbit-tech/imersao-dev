b63865de6d16aaef173ffd293497a40cf7216e42
robbit-tech <56116010+robbit-tech@users.noreply.github.com>
2021-04-09 12:21:46 -0300

    Add files via upload

---
+++ tabela-de-classifica-o/tabela-de-classifica-o/README.markdown
@@ -0,0 +1,5 @@
+# Tabela de classificação
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/robbit-tech/pen/YzNpBmL](https://codepen.io/robbit-tech/pen/YzNpBmL).
+
+
---
+++ tabela-de-classifica-o/tabela-de-classifica-o/dist/script.js
@@ -0,0 +1,60 @@
+var paulo = {nome: "Paulo" , vitorias: 2, empates: 5, derrotas: 1, pontos: 0 }
+
+var rafa = {nome: "Rafa" , vitorias: 1, empates: 5, derrotas: 2, pontos: 0 }
+
+
+function calculaPontos( jogador ){
+   var pontos = (jogador.vitorias * 3 ) + jogador.empates
+  
+   jogador.pontos =  pontos 
+}
+calculaPontos(rafa)
+calculaPontos(paulo)
+
+var jogadores = [rafa,paulo]
+
+exibirJogadoresNaTela(jogadores)
+
+function exibirJogadoresNaTela(jogadores){
+  
+  
+  
+  var html = ""
+  
+  for (var i = 0; i < jogadores.length; i++) {
+        calculaPontos(jogadores[i])  
+        html += "<tr><td>" + jogadores[i].nome     +  "</td>"
+        html += "<td>"    + jogadores[i].vitorias +  "</td>"
+        html += "<td>"    + jogadores[i].empates  +  "</td>"
+        html += "<td>"    + jogadores[i].derrotas +  "</td>"
+        html += "<td>"    + jogadores[i].pontos   +  "</td>"
+        html += "<td><button onClick='adicionarVitoria("+ i + ")'>Vitória</button></td>"
+        html += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+        html += "<td><button onClick='adicionarDerrota("+ i + ")'>Derrota</button></td></tr>"
+      
+  }
+    var tabelaJogadores = document.getElementById('tabelaJogadores')
+    tabelaJogadores.innerHTML = html 
+}
+
+function adicionarVitoria(i){
+  var jogador = jogadores[i]
+  jogador.vitorias++
+  
+  exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i){
+  var jogador = jogadores[i]
+  jogador.derrotas++
+ 
+  exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i){
+  var jogador = jogadores[i]
+  jogador.empates++
+
+  exibirJogadoresNaTela(jogadores)
+  
+}
